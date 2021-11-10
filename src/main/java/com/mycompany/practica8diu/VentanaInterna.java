5c2e00b7627fbeed6113094c854f72fb7471c054
martinvplopez <vanpu3d@gmail.com>
2021-11-10 09:52:45 +0100

    JInternalForm

---
+++ src/main/java/com/mycompany/practica8diu/VentanaInterna.java
@@ -0,0 +1,43 @@
+
+package com.mycompany.practica8diu;
+
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author marti
+ */
+public class VentanaInterna extends javax.swing.JInternalFrame {
+
+
+    public VentanaInterna() {
+        initComponents();
+    }
+
+    protected void open(BufferedImage im){
+        this.setPreferredSize(new Dimension(im.getWidth(), im.getHeight()));
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 394, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 274, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
